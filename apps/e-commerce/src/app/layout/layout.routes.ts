ff2d63d448a48fcaba312295265af4a825a35757
Rajnish Kumar <rajnish6633@gmail.com>
2024-02-10 23:01:43 +0530

    fix : layout and route fixes

--- apps/e-commerce/src/app/layout/layout.routes.ts
+++ apps/e-commerce/src/app/layout/layout.routes.ts
@@ -1,17 +1,60 @@
 import { Route } from '@angular/router';
 import { LayoutComponent } from './layout.component';
+import { authGuard, loginGuard } from '../core/guard';
 
 export const LAYOUT_ROUTES: Route[] = [
   {
     path: '',
     component: LayoutComponent,
-    children:[
+
+    children: [
+      {
+        path: '',
+        redirectTo: 'products',
+        pathMatch: 'full',
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('../components/auth/register/register.component'),
+        canActivate: [loginGuard],
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('../components/auth/login/login.component'),
+        canActivate: [loginGuard],
+      },
+      {
+        path: 'user/verify-token',
+        loadComponent: () =>
+          import(
+            '../components/auth/user-verify-token/user-verify-token.component'
+          ),
+        canActivate: [loginGuard],
+      },
+      {
+        path: 'forget-password',
+        loadComponent: () =>
+          import(
+            '../components/auth/forgot-password/forgot-password.component'
+          ),
+        canActivate: [loginGuard],
+      },
+      {
+        path: 'user/reset-password',
+        loadComponent: () =>
+          import(
+            '../components/auth/forgot-password/forgot-password.component'
+          ),
+        canActivate: [loginGuard],
+      },
       {
         path: 'user',
         loadChildren: () =>
           import('../components/user/user.routes').then(
             (route) => route.USER_ROUTES
           ),
+        canActivate: [authGuard],
       },
       {
         path: 'products',
@@ -19,8 +62,7 @@ export const LAYOUT_ROUTES: Route[] = [
           import('../components/products/products.routes').then(
             (route) => route.PRODUCTS_ROUTES
           ),
-      }
-    ]
+      },
+    ],
   },
- 
 ];
