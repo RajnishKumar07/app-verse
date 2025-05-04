f65e356f4464361672e17a763640f4ebad21ad4f
Rajnish Kumar <rajnish6633@gmail.com>
2025-05-04 15:52:43 +0530

    fix: env fixes

--- apps/e-commerce/src/environments/environment.prod.ts
+++ apps/e-commerce/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://e-commerce-api-nestjs.onrender.com', //nestjs api
+  apiUrl: 'https://costly-luciana-rajnishkumar07-48b23ed7.koyeb.app', //nestjs api
   appUrl: 'https://app-verse.onrender.com',
 };
